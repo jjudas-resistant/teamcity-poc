6014bdab6c1b23e2712b8d85351b9d6b8af11e30
admin <admin@TeamCity>
2023-09-02 09:06:53 +0000

    TeamCity change in 'Transactions' project: 'Unit tests' build configuration was created

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -25,4 +25,10 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2022.10"
 
 project {
+
+    buildType(UnitTests)
 }
+
+object UnitTests : BuildType({
+    name = "Unit tests"
+})
